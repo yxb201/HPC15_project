9839a11ba0c818f100951b6d06afe5c8356378e7
Bill Bao <billbao@linax2.cims.nyu.edu>
2015-05-14 21:20:33 -0400

    modify

--- start.c
+++ start.c
@@ -14,6 +14,35 @@ int mod(int a, int b){
 	
 }
 
+
+void setSbuffer(double *buffer, double *rect, int x[3], int dimbuffer[3], int dimrect[3] ){
+	
+	int i,j,k;
+	
+	for (k=0; k<dimbuffer[2]; ++k){
+		for (j=0; j<dimbuffer[1]; ++j){
+			for (i=0; i<dimbuffer[0]; ++i){
+				buffer[l(i,j,k,dimbuffer[0], dimbuffer[1])] = rect[l(x[0]+i,x[1]+j,x[2]+k, dimrect[0], dimrect[1])];	
+			}
+		}
+	}
+
+}
+
+
+void getRbuffer(double *buffer, double *rect, int x[3], int dimbuffer[3], int dimrect[3]){
+	int i,j,k;
+
+	for (k=0; k<dimbuffer[2]; ++k){
+		for (j=0; j<dimbuffer[1]; ++j){
+			for (i=0; i<dimbuffer[0]; ++i){
+				rect[l(x[0]+i,x[1]+j,x[2]+k,dimrect[0],dimrect[1])] += buffer[l(i,j,k,dimbuffer[0], dimbuffer[1])];
+			}
+		}
+	}
+}
+
+
 int main(int argc, char *argv[])
 {	
 	int nproc, proc_id, proc_i, proc_j, conf; 
@@ -24,13 +53,14 @@ int main(int argc, char *argv[])
 	int istart[3], isize[3], iend[3];
 	int fstart[3], fsize[3], fend[3];
 	double *spread_rect, *local_rect;	
-	int Msp = 2; 
+	int Msp = 3; 
 	int mx, my, mz, lmx, lmy, lmz, smx, smy, smz;
 	int NORTH, NE, EAST, SE, SOUTH, SW, WEST, NW, P1, P2;
 	int i,j,k;
  	double *N_Recv, *NE_Recv, *E_Recv, *SE_Recv, *S_Recv, *SW_Recv, *W_Recv, *NW_Recv;
 	double *N_Send, *NE_Send, *E_Send, *SE_Send, *S_Send, *SW_Send, *W_Send, *NW_Send;	
-
+	int dimSbuffer[3], dimRbuffer[3], dimSpreadRect[3];
+	int idx[3]; 
 	L = 2.0 * M_PI;
 
 
@@ -54,11 +84,6 @@ int main(int argc, char *argv[])
 	SW    = mod(k-1,P2)*P1 + mod(j-1,P1);
 	WEST  = mod(k  ,P2)*P1 + mod(j-1,P1);
 	NW	  = mod(k+1,P2)*P1 + mod(j-1,P1);
-/* 
-if (proc_id == 11){
-	printf("N=%2d, NE=%2d, E=%2d, SE=%2d, S =%2d, SW=%2d, W=%2d, NW=%2d\n", NORTH,NE,EAST,SE,SOUTH,SW,WEST,NW);
-}
-*/
 
 	// assume for now 
 	nx = ny = nz = 12;
@@ -103,99 +128,216 @@ if (proc_id == 11){
 	SE_Recv = (double *) malloc( sizeof(double) * lnx*Msp*Msp );
 	SE_Send = (double *) malloc( sizeof(double) * lnx*Msp*Msp );
 
-	
-if (proc_id == 7){
 
-	
+
+//if (proc_id == 9){
+/*
 	printf("istart[0] = %d, istart[1] = %d, istart[2] = %d\n", istart[0],istart[1], istart[2]);
 	printf("iend[0] = %d, iend[1] = %d, iend[2] = %d\n", iend[0],iend[1], iend[2]);
 	printf("isize[0] = %d, isize[1] = %d, isize[2] = %d\n", isize[0],isize[1], isize[2]);
-	
-	// generate a source 
+*/	
+	// generate a source at the center of each processor 
 	double xj[3];
-	xj[0]=6.5*h; xj[1]=6.5*h; xj[2]=6.5*h;
+	xj[0]= (istart[0]); 
+	xj[1]= (istart[1]+iend[1])*h/2; 
+	xj[2]= (istart[2]+iend[2])*h/2;
 	
 	mx = (int) (xj[0]/h); 
 	my = (int) (xj[1]/h);
 	mz = (int) (xj[2]/h);
-	
-	printf("center: %d %d %d \n", mx, my, mz);
 
+if (proc_id == 11){	
+	printf("center: %d %d %d \n", mx, my, mz);
+}
 	// mx,my,mz in the spread_rect
-	smx= my - (istart[0]-1);
+	smx= mx - (istart[0]-1);
 	smy= my - (istart[1]-1) + Msp;
 	smz= mz - (istart[2]-1) + Msp;
 
-	printf("lnx = %d, lny = %d, lnz = %d \n", lnx, lny, lnz);
-	printf("smx = %d, smy = %d, smz = %d \n", smx, smy, smz);
+//	printf("lnx = %d, lny = %d, lnz = %d \n", lnx, lny, lnz);
+//	printf("smx = %d, smy = %d, smz = %d \n", smx, smy, smz);
 
+	dimSpreadRect[0]=lnx; dimSpreadRect[1]=lny+2*Msp; dimSpreadRect[2] = lnz+2*Msp;
+
+	// build the spreading rectangle
 	for (k=-Msp+1; k<=Msp; ++k){
-	for (j=-Msp+1; j<=Msp; ++j){
-	for (i=-Msp+1; i<=Msp; ++i){ 
-
-//		printf("(i,j,k) = (%d, %d, %d) \n", i+smx, j+smy, k+smz);
-		spread_rect[l(mod(i+smx,nx),j+smy,k+smz,nx,lny+2*Msp)] += 1.;
-	}
-	}
+		for (j=-Msp+1; j<=Msp; ++j){
+			for (i=-Msp+1; i<=Msp; ++i){ 
+				spread_rect[l(mod(i+smx,nx),j+smy,k+smz,lnx,lny+2*Msp)] += 1.;
+			}
+		}
 	}
 
+//}
+
+
+	// copy spreading rectangle to local rectangle
+	idx[0] = 0; idx[1] = Msp; idx[2] = Msp;
+	setSbuffer(local_rect, spread_rect, idx, isize, dimSpreadRect );
+
+
+	// set North Send buffer
+	idx[0] = 0; idx[1]=Msp; idx[2]=lnz+Msp;
+	dimSbuffer[0] = lnx; dimSbuffer[1]=lny; dimSbuffer[2]=Msp; 	
+	setSbuffer(N_Send, spread_rect, idx, dimSbuffer, dimSpreadRect);
+
+	// set South Send buffer
+	idx[0] = 0; idx[1]=Msp; idx[2]=0; 	
+	setSbuffer(S_Send, spread_rect, idx, dimSbuffer, dimSpreadRect);
+
+	// set West Send buffer
+	idx[0] = 0; idx[1]=0; idx[2]=Msp;
+	dimSbuffer[0] = lnx; dimSbuffer[1]=Msp; dimSbuffer[2]=lnz; 	
+	setSbuffer(W_Send, spread_rect, idx, dimSbuffer, dimSpreadRect);
+	
+	// set East Send buffer
+	idx[0] = 0; idx[1]=lny+Msp; idx[2]=Msp; 	
+	setSbuffer(E_Send, spread_rect, idx, dimSbuffer, dimSpreadRect);
+
+	// set NE Send buffer
+	idx[0] = 0; idx[1]=lny+Msp; idx[2]=lnz+Msp;
+	dimSbuffer[0] = lnx; dimSbuffer[1]=Msp; dimSbuffer[2]=Msp; 	
+	setSbuffer(NE_Send, spread_rect, idx, dimSbuffer, dimSpreadRect);
+
+	// set SE send buffer
+	idx[0] = 0; idx[1]=lny+Msp; idx[2]=0; 	
+	setSbuffer(SE_Send, spread_rect, idx, dimSbuffer, dimSpreadRect);
+
+	// set SW send buffer
+	idx[0] = 0; idx[1]=0; idx[2]=0; 	
+	setSbuffer(SW_Send, spread_rect, idx, dimSbuffer, dimSpreadRect);
+
+	// set NW send buffer
+	idx[0] = 0; idx[1]=0; idx[2]=lnz+Msp; 	
+	setSbuffer(NW_Send, spread_rect, idx, dimSbuffer, dimSpreadRect);
+
+
+/*
+	
+	printf("center: %d %d %d \n", mx, my, mz);
+
+	for(k=0; k<lnz; ++k){
+		for(j=0; j<lny; ++j){
+			for(i=0; i<lnx; ++i){
+				printf("%1.1f ", local_rect[l(i,j,k,lnx,lny)]);
+			}
+			printf("\n");
+		}
+		printf("\n\n");
+	}
+*/
+
+/*
 	for (k=0; k < Msp; ++k){
 		for (j=0; j<Msp; ++j){
 			for (i=0; i<lnx; ++i){
-				SE_Send[l(i,j,k,nx,Msp)] = spread_rect[l(0+i,Msp+lny+j,0+k,nx, lny+2*Msp)];
+//				SE_Send[l(i,j,k,nx,Msp)] = spread_rect[l(0+i,Msp+lny+j,0+k,nx, lny+2*Msp)];
 				printf("%1.1f ", SE_Send[l(i,j,k,nx,Msp)]);
 			}
 			printf("\n");
 		}
 		printf("\n\n\n");
 	}
-
-	MPI_Send(SE_Send, lnx*Msp*Msp, MPI_DOUBLE, SE, 99, MPI_COMM_WORLD);
-
-/*
-	
-	for (k=1; k<=4; ++k){
-	for (j=3; j<=6; ++j){
-	for (i=5; i<=8; ++i){
-
-//		spread_rect[l(i,j,k,nx,lny+2*Msp)] = 1;		
-//		printf("(i,j,k) = (%d, %d, %d) \n", i, j, k);
-		printf("%1.2f ", spread_rect[l(i,j,k,nx,lny+2*Msp)]);
-//		printf("%1.2f ", spread_rect[i + nx*j + nx*(lny+2*Msp)*k]);
-	}
-		printf("\n");
-	}
-		printf("\n \n \n");
-	}
-
-//	double tmp = 0;
-	for (k=0; k<lnz+2*Msp; ++k){
-	for (j=0; j<lny+2*Msp; ++j){
-	for (i=0; i<lnx ; ++i){
-//		tmp = tmp + spread_rect[l(i,j,k,nx,lny+2*Msp)];
-		printf("%1.1f ", spread_rect[l(i,j,k,nx,lny+2*Msp)]);
-	}
-		printf("\n");
-	}
-		printf("\n \n \n ");
-	}
-//	printf("final sum = %f\n", tmp);	
-
 */
+
+
+	// MPI send to neighbours
+	MPI_Send( N_Send, lnx*lny*Msp, MPI_DOUBLE, NORTH, 1, MPI_COMM_WORLD);
+	MPI_Send( S_Send, lnx*lny*Msp, MPI_DOUBLE, SOUTH, 2, MPI_COMM_WORLD);
+	MPI_Send( W_Send, lnx*Msp*lnz, MPI_DOUBLE,  WEST, 3, MPI_COMM_WORLD);
+	MPI_Send( E_Send, lnx*Msp*lnz, MPI_DOUBLE,  EAST, 4, MPI_COMM_WORLD);
+	MPI_Send(NE_Send, lnx*Msp*Msp, MPI_DOUBLE,    NE, 5, MPI_COMM_WORLD);
+	MPI_Send(SE_Send, lnx*Msp*Msp, MPI_DOUBLE,    SE, 6, MPI_COMM_WORLD);
+	MPI_Send(SW_Send, lnx*Msp*Msp, MPI_DOUBLE,    SW, 7, MPI_COMM_WORLD);
+	MPI_Send(NW_Send, lnx*Msp*Msp, MPI_DOUBLE,    NW, 8, MPI_COMM_WORLD);
+
+
+	// MPI receive from neighbours
+	MPI_Recv( N_Recv, lnx*lny*Msp, MPI_DOUBLE, NORTH, 2, MPI_COMM_WORLD, &status);
+	MPI_Recv( S_Recv, lnx*lny*Msp, MPI_DOUBLE, SOUTH, 1, MPI_COMM_WORLD, &status);
+	MPI_Recv( W_Recv, lnx*Msp*lnz, MPI_DOUBLE,  WEST, 4, MPI_COMM_WORLD, &status);
+	MPI_Recv( E_Recv, lnx*Msp*lnz, MPI_DOUBLE,  EAST, 3, MPI_COMM_WORLD, &status);
+	MPI_Recv(NE_Recv, lnx*Msp*Msp, MPI_DOUBLE,    NE, 7, MPI_COMM_WORLD, &status);
+	MPI_Recv(SE_Recv, lnx*Msp*Msp, MPI_DOUBLE,    SE, 8, MPI_COMM_WORLD, &status);
+	MPI_Recv(SW_Recv, lnx*Msp*Msp, MPI_DOUBLE,    SW, 5, MPI_COMM_WORLD, &status);
+	MPI_Recv(NW_Recv, lnx*Msp*Msp, MPI_DOUBLE,    NW, 6, MPI_COMM_WORLD, &status);
+
+
+	// copy receive buffer to local rectangle
+	// add contribution from N buffer
+	idx[0]=0; idx[1]=0; idx[2]=lnz-Msp;
+	dimRbuffer[0]=lnx; dimRbuffer[1]=lny; dimRbuffer[2]=Msp; 
+	getRbuffer( N_Recv, local_rect, idx, dimRbuffer, isize );
+
+	// add contribution from S buffer
+	idx[0]=0; idx[1]=0; idx[2]=0;
+	getRbuffer( S_Recv, local_rect, idx, dimRbuffer, isize );
+
+	// add contribution from W buffer
+	idx[0]=0; idx[1]=0; idx[2]=0;
+	dimRbuffer[0]=lnx; dimRbuffer[1]=Msp; dimRbuffer[2]=lnz;
+	getRbuffer( W_Recv, local_rect, idx, dimRbuffer, isize );
+	
+	// add contribution from E buffer
+	idx[0]=0; idx[1]=lny-Msp; idx[2]=0;
+	getRbuffer( E_Recv, local_rect, idx, dimRbuffer, isize );
+
+	// add contribution form NW buffer 
+	idx[0]=0; idx[1]=0; idx[2]=lnz-Msp;
+	dimRbuffer[0]=lnx; dimRbuffer[1]=Msp; dimRbuffer[2]=Msp; 
+	getRbuffer( NW_Recv, local_rect, idx, dimRbuffer, isize );
+
+	// add contribution from SW buffer 
+	idx[0]=0; idx[1]=0; idx[2]=0;
+	getRbuffer( SW_Recv, local_rect, idx, dimRbuffer, isize );
+
+	// add contribution from SE buffer
+	idx[0]=0; idx[1]=lny-Msp; idx[2]=0;
+	getRbuffer( SE_Recv, local_rect, idx, dimRbuffer, isize );
+
+	// add contribution from NE buffer
+	idx[0]=0; idx[1]=lny-Msp; idx[2]=lnz-Msp;
+	getRbuffer( NE_Recv, local_rect, idx, dimRbuffer, isize );
+
+
+
+if (proc_id == 2){ 
+
+	for(k=0; k<lnz; ++k){
+		for(j=0; j<lny; ++j){
+			for(i=0; i<lnx; ++i){
+				printf("%1.1f ", local_rect[l(i,j,k,lnx,lny)]);
+			}
+			printf("\n");
+		}
+		printf("\n\n");
+	}
+
 }
-
-
+/*
 if (proc_id == 5){
 	
 	printf("NW=%d\n",NW);
 	MPI_Recv(NW_Recv, lnx*Msp*Msp, MPI_DOUBLE, NW, 99, MPI_COMM_WORLD, &status);
-	for(i=0; i<lnx*Msp*Msp; ++i)
-		printf("%1.1f ", NW_Recv[i]);	
 	
+
+	idx[0] = 0; idx[1]=0; idx[2]=isize[2]-Msp;
+	dimRbuffer[0]=lnx; dimRbuffer[1]=Msp; dimRbuffer[2]=Msp;
+	copyRbuffer(NW_Recv, local_rect, idx, dimRbuffer, isize );
+
+	for(k=0; k<isize[2];++k){
+	for(j=0; j<isize[1];++j){
+	for(i=0; i<isize[0];++i){
+		printf("%1.1f ", local_rect[l(i,j,k,isize[0],isize[1])]);	
+	}
+	printf("\n");
+	}
+	printf("\n\n");
+	}
 	printf("received\n");
 
 }
-
+*/
 
 	Cp3dfft_clean();
 	
